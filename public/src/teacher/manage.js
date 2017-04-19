4c49f058d3952d96f5a199cec34a3b0165e05820
xiaoduo91 <1500257054@qq.com>
2017-04-19 19:28:23 +0800

    讲师

--- public/src/teacher/manage.js
+++ public/src/teacher/manage.js
@@ -1,4 +1,17 @@
 define(['jquery', '../utils'], function ($, utils) {
 
-    utils.setMenu('/teacher/manage');
+    utils.setMenu('/teacher/index');
+    $('#teacherManage').on('submit', function(){
+    	alert(1);
+    	var formData=$(this).serialize();
+          $.ajax({
+          	url:"/api/teacher/add",
+          	type:'post',
+          	success: function (info) {
+                console.log(info);
+            }
+          })
+    	
+    	return false;
+    })
  });  
